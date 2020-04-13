1a4616ddb89a51cae496f5d1aeb69863cdd6c511
sealocal <local.mat@gmail.com>
2020-04-13 01:08:51 -0700

    add support for running script in docker

--- README.md
+++ README.md
@@ -8,17 +8,44 @@ The backup script requires the AWS CLI.
 
 ### Configure environment variables
 
+AWS keys
+
+    AWS_ACCESS_KEY_ID=someaccesskey
+    AWS_SECRET_ACCESS_KEY=supermegasecret
+
+Name of the region and bucket (and, optional path) to which the backup files will be uploaded.
+
+    AWS_DEFAULT_REGION=us-east-1
+    S3_BUCKET_PATH=your-bucket[/prefix]
+
+Specify a database URL for
+
+    DATABASE_URL=postgres://user:pass@host/source_database
+
+String for the name of the backup file; timestamps will be appended to the file name.
+
+    DBNAME=source_database
+
+Set a password for the encrypted backup.
+
+    DB_BACKUP_ENC_KEY=password
+
+# Example Docker Usage
+
 ```
-DBNAME=source_database
-AWS_ACCESS_KEY_ID=someaccesskey
-AWS_SECRET_ACCESS_KEY=supermegasecret
-AWS_DEFAULT_REGION=eu-central-1
-S3_BUCKET_PATH=your-bucket
-DATABASE_URL=postgres://user:pass@host/source_database
-DB_BACKUP_ENC_KEY=password
+docker build -t pg_dump_to_s3 .
+docker run --rm -it \
+-e AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) \
+-e AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) \
+-e AWS_DEFAULT_REGION=$(aws configure get region) \
+-e S3_BUCKET_PATH=$S3_BUCKET_PATH \
+-e DATABASE_URL=$DATABASE_URL \
+-e DBNAME=source_database \
+-e DB_BACKUP_ENC_KEY=$DB_BACKUP_ENC_KEY \
+pg_dump_to_s3
 ```
 
-### Usage
+# Example Local Usage
 
 ```bash
 bash /app/vendor/backup.sh --dbname <string_for_name>
--- backup.sh
+++ backup.sh
@@ -56,6 +56,12 @@ fi
 
 printf "${Green}Dump backup of DATABASE_URL ...${EC}\n"
 
+if [ -f /.dockerenv ]; then
+  readonly download_path='/tmp'
+else
+  readonly download_path='./'
+fi
+
 # Borrow pg_dump flags from pg:dump per heroku support article
 # https://help.heroku.com/7U1BTYHB/how-can-i-take-a-logical-backup-of-large-heroku-postgres-databases
 
@@ -64,50 +70,50 @@ printf "dump plain format ...\n"
 time pg_dump \
   --no-acl --no-owner --quote-all-identifiers \
   --format=plain --compress=4 \
-  $DATABASE_URL > ./"${FILENAME}"_plain_format.sql.gz
+  $DATABASE_URL > $download_path/"${FILENAME}"_plain_format.sql.gz
 
 printf "Encrypt the plain format backup ...\n"
 openssl enc -aes-256-cbc -pbkdf2 -e -pass "env:DB_BACKUP_ENC_KEY" \
-  -in ./"${FILENAME}"_plain_format.sql.gz \
+  -in $download_path/"${FILENAME}"_plain_format.sql.gz \
   -out /tmp/"${FILENAME}"_plain_format.gz.enc
 
 printf "dump custom format ...\n"
 time pg_dump \
   --no-acl --no-owner --quote-all-identifiers \
   --format=custom \
-  $DATABASE_URL > ./"${FILENAME}"_custom_format.dump
+  $DATABASE_URL > $download_path/"${FILENAME}"_custom_format.dump
 
 printf "Encrypt the custom format backup"
 openssl enc -aes-256-cbc -pbkdf2 -e -pass "env:DB_BACKUP_ENC_KEY" \
-  -in ./"${FILENAME}"_custom_format.dump \
+  -in $download_path/"${FILENAME}"_custom_format.dump \
   -out /tmp/"${FILENAME}"_custom_format.enc
 
 printf "dump directory format ...\n"
 time pg_dump \
   --no-acl --no-owner --quote-all-identifiers \
   --format=directory \
-  --file=./"${FILENAME}"_directory_format $DATABASE_URL
+  --file=$download_path/"${FILENAME}"_directory_format $DATABASE_URL
 
 printf "compress directory format ...\n"
-tar -zcvf ./"${FILENAME}"_directory_format.tar.gz ./"${FILENAME}"_directory_format/
+tar -zcvf $download_path/"${FILENAME}"_directory_format.tar.gz $download_path/"${FILENAME}"_directory_format/
 
 printf "Encrypt the directory format backup ...\n"
 openssl enc -aes-256-cbc -pbkdf2 -e -pass "env:DB_BACKUP_ENC_KEY" \
-  -in ./"${FILENAME}"_directory_format.tar.gz \
+  -in $download_path/"${FILENAME}"_directory_format.tar.gz \
   -out /tmp/"${FILENAME}"_directory_format.gz.enc
 
 printf "dump tar format ...\n"
 time pg_dump \
   --no-acl --no-owner --quote-all-identifiers \
   --format=tar \
-  --file=./"${FILENAME}"_tar_format.tar $DATABASE_URL
+  --file=$download_path/"${FILENAME}"_tar_format.tar $DATABASE_URL
 
 printf "compress tar format ...\n"
-gzip ./"${FILENAME}"_tar_format.tar
+gzip $download_path/"${FILENAME}"_tar_format.tar
 
 printf "Encrypt the tar format backup ...\n"
 openssl enc -aes-256-cbc -pbkdf2 -e -pass "env:DB_BACKUP_ENC_KEY" \
-  -in ./"${FILENAME}"_tar_format.tar.gz \
+  -in $download_path/"${FILENAME}"_tar_format.tar.gz \
   -out /tmp/"${FILENAME}"_tar_format.gz.enc
 
 printf "${Green}Copy Postgres dumps to AWS S3 at S3_BUCKET_PATH...${EC}\n"
@@ -132,7 +138,9 @@ time aws s3 cp \
   s3://$S3_BUCKET_PATH/$DBNAME/"${FILENAME}"_tar_format.gz.enc
 
 # Remove the database dumps from the app server
-rm -v /tmp/"${FILENAME}"_plain_format.gz.enc ./"${FILENAME}"_plain_format.sql.gz
-rm -v /tmp/"${FILENAME}"_custom_format.enc ./"${FILENAME}"_custom_format.dump
-rm -rv /tmp/"${FILENAME}"_directory_format.gz.enc ./"${FILENAME}"_directory_format.tar.gz ./"${FILENAME}"_directory_format
-rm -v /tmp/"${FILENAME}"_tar_format.gz.enc ./"${FILENAME}"_tar_format.tar.gz
+rm -v /tmp/"${FILENAME}"_plain_format.gz.enc $download_path/"${FILENAME}"_plain_format.sql.gz
+rm -v /tmp/"${FILENAME}"_custom_format.enc $download_path/"${FILENAME}"_custom_format.dump
+rm -rv /tmp/"${FILENAME}"_directory_format.gz.enc $download_path/"${FILENAME}"_directory_format.tar.gz $download_path/"${FILENAME}"_directory_format
+rm -v /tmp/"${FILENAME}"_tar_format.gz.enc $download_path/"${FILENAME}"_tar_format.tar.gz
+
+printf '\nDone!\n'
